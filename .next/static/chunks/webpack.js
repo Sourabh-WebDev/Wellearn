e91f46060fdfea5920228a506a184e71295c95ca
Sourabh Verma <vermaanurag5542@gmail.com>
2023-06-21 15:33:27 +0530

    ok

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4260d78bb9a6959d"; }
+/******/ 		__webpack_require__.h = function() { return "022bd28541f050ba"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
