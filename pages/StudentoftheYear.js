dc61cb998bd06d598bbb2544ab3381c91e33b9ad
Sourabh Verma <vermaanurag5542@gmail.com>
2023-06-24 13:37:18 +0530

    Update StudentoftheYear.js

--- pages/StudentoftheYear.js
+++ pages/StudentoftheYear.js
@@ -42,7 +42,8 @@ const StudentoftheYear = () => {
                 <div className="containerForSOY">
                     <div className="card cardSOY">
                         <div className="image text-center">
-                            <img src={`${config.backendUrl}/api/instructorofmonths/${instructorOfMonth ? instructorOfMonth.img : ""}`} />
+                            {/* <img src={`${config.backendUrl}/api/instructorofmonths/${instructorOfMonth ? instructorOfMonth.img : ""}`} /> */}
+                            <img src={'https://www.educationtopia.net/wp-content/uploads/2021/01/teacher.jpg'} alt="Instructor Of The Month" />
                             <h5 className='text-nowrap'>Instructor Of The Month 🏆</h5>
                         </div>
                         <div className='content'>
